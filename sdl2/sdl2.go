012bb7cd4a7449620c9eeebf303687027adbb275
realjf <real_jf@hotmail.com>
2021-09-14 18:00:24 +0800

    update

--- sdl2/sdl2.go
+++ sdl2/sdl2.go
@@ -1,28 +1,43 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
 	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		800, 600, sdl.WINDOW_SHOWN)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
 	defer window.Destroy()
 
-	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	surface, err := window.GetSurface()
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
-	defer renderer.Destroy()
+	surface.FillRect(nil, 0)
 
-	sdl.Delay(2000)
+	rect := sdl.Rect{0, 0, 200, 200}
+	surface.FillRect(&rect, 0xffff0000)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
 
 }
