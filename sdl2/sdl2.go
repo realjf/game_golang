c2af90514843f74f3c05f067e29085d1045d8437
realjf <real_jf@hotmail.com>
2021-09-14 20:45:29 +0800

    update

--- sdl2/sdl2.go
+++ sdl2/sdl2.go
@@ -1,43 +1,91 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-func main() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
+const winWidth, winHeight int = 800, 600
+
+type color struct {
+	r, g, b byte
+}
+
+func setPixel(x, y int, c color, pixels []byte) {
+	index := (y*winWidth + x) * 4
+
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
 	}
-	defer sdl.Quit()
+
+}
+
+func main() {
+	// if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+	// 	panic(err)
+	// }
+	// defer sdl.Quit()
+	winWidth := 800
+	winHeight := 600
 
 	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
+		int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
 
 	if err != nil {
 		panic(err)
 	}
 	defer window.Destroy()
 
-	surface, err := window.GetSurface()
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
-	surface.FillRect(nil, 0)
+	defer renderer.Destroy()
 
-	rect := sdl.Rect{0, 0, 200, 200}
-	surface.FillRect(&rect, 0xffff0000)
-	window.UpdateSurface()
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tex.Destroy()
 
-	running := true
-	for running {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				println("Quit")
-				running = false
-				break
-			}
+	pixels := make([]byte, winWidth*winHeight*4)
+
+	for y := 0; y < winHeight; y++ {
+		for x := 0; x < winWidth; x++ {
+			setPixel(x, y, color{255, 0, 0}, pixels)
 		}
 	}
+	tex.Update(nil, pixels, winWidth*4)
+	renderer.Copy(tex, nil, nil)
+	renderer.Present()
+
+	sdl.Delay(2000)
+
+	// surface, err := window.GetSurface()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// surface.FillRect(nil, 0)
+
+	// rect := sdl.Rect{0, 0, 200, 200}
+	// surface.FillRect(&rect, 0xffff0000)
+	// window.UpdateSurface()
+
+	// running := true
+	// for running {
+	// 	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+	// 		switch event.(type) {
+	// 		case *sdl.QuitEvent:
+	// 			println("Quit")
+	// 			running = false
+	// 			break
+	// 		}
+	// 	}
+	// }
 
 }
